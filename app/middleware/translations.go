ec1775999ba15c8d5bb545a2b8e1580566bef421
jinxing <821901008@qq.com>
2023-02-12 22:48:57 +0800

    feat: 验证修改

--- app/middleware/translations.go
+++ app/middleware/translations.go
@@ -7,36 +7,41 @@ import (
 	"github.com/go-playground/locales/zh"
 	"github.com/go-playground/locales/zh_Hant_TW"
 	ut "github.com/go-playground/universal-translator"
-	validator "github.com/go-playground/validator/v10"
+	"github.com/go-playground/validator/v10"
 	enTranslations "github.com/go-playground/validator/v10/translations/en"
 	zhTranslations "github.com/go-playground/validator/v10/translations/zh"
 	zhTwTranslations "github.com/go-playground/validator/v10/translations/zh_tw"
 )
 
-func Translations() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		uni := ut.New(en.New(), zh.New(), zh_Hant_TW.New())
-		locale := c.GetHeader("locale")
-		trans, _ := uni.GetTranslator(locale)
-		v, ok := binding.Validator.Engine().(*validator.Validate)
-		if ok {
-			switch locale {
-			case "zh":
-				_ = zhTranslations.RegisterDefaultTranslations(v, trans)
-			case "en":
-				_ = enTranslations.RegisterDefaultTranslations(v, trans)
-			case "zh-tw":
-				fallthrough
-			case "zh-TW":
-				fallthrough
-			case "tw":
-				_ = zhTwTranslations.RegisterDefaultTranslations(v, trans)
-			default:
-				_ = zhTranslations.RegisterDefaultTranslations(v, trans)
-			}
-
-			c.Set("trans", trans)
+func NewUniversalTranslator() *ut.UniversalTranslator {
+	uni := ut.New(zh.New(), en.New(), zh.New(), zh_Hant_TW.New())
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		if trans, found := uni.GetTranslator("en"); found {
+			_ = enTranslations.RegisterDefaultTranslations(v, trans)
 		}
+
+		if trans, found := uni.GetTranslator("zh"); found {
+			_ = zhTranslations.RegisterDefaultTranslations(v, trans)
+		}
+
+		if trans, found := uni.GetTranslator("zh_Hant_TW"); found {
+			_ = zhTwTranslations.RegisterDefaultTranslations(v, trans)
+		}
+	}
+
+	return uni
+}
+
+func Translations() gin.HandlerFunc {
+	uni := NewUniversalTranslator()
+	return func(c *gin.Context) {
+		locale := c.GetHeader("locale")
+		if locale == "zh-tw" || locale == "zh-TW" || locale == "tw" {
+			locale = "zh_Hant_TW"
+		}
+
+		trans, _ := uni.GetTranslator(locale)
+		c.Set("trans", trans)
 
 		c.Next()
 	}
